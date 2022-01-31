d0fa6a077438d304642885e55997f2ea82e14629
Violeta P <32481414+violeta-p@users.noreply.github.com>
2022-01-31 10:14:10 +0100

    Add model testing for Movie

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -9,5 +9,30 @@ RSpec.describe Movie, type: :model do
     end
 
     it { expect(subject).to be_valid }
+
+    it 'not valid without title' do
+      subject.title = ''
+      expect(subject).to_not be_valid
+    end
+
+    it 'not valid without description' do
+      subject.description = ''
+      expect(subject).to_not be_valid
+    end
+
+    it 'not valid without duration' do
+      subject.duration = ''
+      expect(subject).to_not be_valid
+    end
+
+    it 'not valid without release_date' do
+      subject.release_date = ''
+      expect(subject).to_not be_valid
+    end
+
+    it 'not valid without cast' do
+      subject.cast = ''
+      expect(subject).to_not be_valid
+    end
   end
 end
