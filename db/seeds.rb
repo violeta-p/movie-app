83eb07580dbcfa5cf2eacb0639921a948b91ed30
Violeta <32481414+violeta-p@users.noreply.github.com>
2022-02-12 11:42:50 +0100

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |i|
+  Movie.create!(
+    title: "Title #{i}",
+    duration: "1h #{i}8 minutes",
+    cast: "cast for movie #{i}",
+    description: "very nice movie",
+    release_date: Date.current
+  )
+end
