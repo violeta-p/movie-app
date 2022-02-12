4b52975232e375796b2608580a8aa7540afde51b
Violeta <32481414+violeta-p@users.noreply.github.com>
2022-02-12 11:19:29 +0100

    change db from sqlite to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_11_08_154851) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.text "description"
